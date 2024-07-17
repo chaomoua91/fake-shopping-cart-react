5c1611c41e9fe000219c20f87c5e94c848732b90
Chao Moua <chaomoua91@gmail.com>
2024-07-17 18:55:09 -0500

    setting up my different "pages"

---
+++ src/routes.jsx
@@ -0,0 +1,38 @@
+import App from "./components/App";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import About from "./components/About";
+import ErrorPage from "./components/ErrorPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "about",
+        element: <About />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+];
+
+export default routes;
