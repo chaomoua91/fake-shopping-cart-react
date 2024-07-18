3dec08fe4a769479ae4ffdb54aacef86831a25d5
Chao Moua <chaomoua91@gmail.com>
2024-07-18 17:56:37 -0500

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   const [cartCount, setCartCount] = useState(0);
